e7f23c51f9596b9700568586a277bc2f505a002c
megan <mdptn@users.noreply.github.com>
2016-10-06 20:26:03 -0700

    initiallization of asg1 for cs112

---
+++ CMPS112/asg1/sbi.scm
@@ -0,0 +1,73 @@
+;;------------------------------------------------------------------
+;; sbi.scm
+;; Assignment 1
+;; CMPS 112 Fall 2016
+;; Megan Nguyen
+;; mednguye@ucsc.edu
+;; Code Template Provided by Professor Mackey in the CMPS112 files
+;;------------------------------------------------------------------
+
+
+
+(define *stderr* (current-error-port))
+
+(define *run-file*
+    (let-values
+        (((dirpath basepath root?)
+            (split-path (find-system-path 'run-file))))
+        (path->string basepath))
+)
+
+(define (die list)
+    (for-each (lambda (item) (display item *stderr*)) list)
+    (newline *stderr*)
+    (exit 1)
+)
+
+(define (usage-exit)
+    (die `("Usage: " ,*run-file* " filename"))
+)
+
+(define (readlist-from-inputfile filename)
+    (let ((inputfile (open-input-file filename)))
+         (if (not (input-port? inputfile))
+             (die `(,*run-file* ": " ,filename ": open failed"))
+             (let ((program (read inputfile)))
+                  (close-input-port inputfile)
+                         program))))
+
+(define (write-program-by-line filename program)
+    (printf "==================================================~n")
+    (printf "~a: ~s~n" *run-file* filename)
+    (printf "==================================================~n")
+    (printf "(~n")
+    (map (lambda (line) (printf "~s~n" line)) program)
+    (printf ")~n"))
+
+
+;;*function-table*--------------------------------------------------
+;;  holds all of the functions, which include the operators. 
+;;------------------------------------------------------------------
+
+
+;;*label-table*--------------------------------------------------
+;;  holds the addresses of each line, one level up from statements.
+;;------------------------------------------------------------------
+
+
+;;*variable-table*--------------------------------------------------
+;;  holds the value of all variables.
+;;------------------------------------------------------------------
+
+
+
+(define (main arglist)
+    (if (or (null? arglist) (not (null? (cdr arglist))))
+        (usage-exit)
+        (let* ((sbprogfile (car arglist))
+               (program (readlist-from-inputfile sbprogfile)))
+              (write-program-by-line sbprogfile program))))
+
+(main (vector->list (current-command-line-arguments)))
+
+
