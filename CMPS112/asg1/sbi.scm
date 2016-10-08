582735e641694adce364bad57bc15b6023c2d592
Megan Nguyen <mdptnguyen@gmail.com>
2016-10-07 21:28:38 -0700

    analyzed mackey's code and added comments to the functions.
    understand scheme alot better now

--- CMPS112/asg1/sbi.scm
+++ CMPS112/asg1/sbi.scm
@@ -1,5 +1,7 @@
+#!/afs/cats.ucsc.edu/courses/cmps112-wm/usr/racket/bin/mzscheme -qr
 ;;------------------------------------------------------------------
 ;; sbi.scm
+;; Silly Basic Interpreter
 ;; Assignment 1
 ;; CMPS 112 Fall 2016
 ;; Megan Nguyen
@@ -8,9 +10,14 @@
 ;;------------------------------------------------------------------
 
 
-
+;; defines *stderr* as the output port for errors and logging
+;; provided by Mackey
 (define *stderr* (current-error-port))
 
+
+;; defines *run-file* as sbi.scm
+;; basically, it deconstructs the file path into a smaller path, sbi.scm.
+;; provided by Mackey
 (define *run-file*
     (let-values
         (((dirpath basepath root?)
@@ -18,16 +25,26 @@
         (path->string basepath))
 )
 
+
+;; defines the function "die" with a list parameter.
+;; displays each item in the list passed through to *stderr*
+;; provided by Mackey
 (define (die list)
     (for-each (lambda (item) (display item *stderr*)) list)
     (newline *stderr*)
     (exit 1)
 )
 
+
+;; calls the die function to run the error message, "Usage: sbi.scm filename"
+;; provided by Mackey
 (define (usage-exit)
     (die `("Usage: " ,*run-file* " filename"))
 )
 
+
+;; reads the input file, if unable to read then it throws an error with die
+;; provided by Mackey
 (define (readlist-from-inputfile filename)
     (let ((inputfile (open-input-file filename)))
          (if (not (input-port? inputfile))
@@ -36,6 +53,10 @@
                   (close-input-port inputfile)
                          program))))
 
+
+;; the function write-program-by-line first displays the *run-file* and filename
+;; then each line of the file is printed out
+;; provided by Mackey
 (define (write-program-by-line filename program)
     (printf "==================================================~n")
     (printf "~a: ~s~n" *run-file* filename)
