8b1cb52b880c98801a8933eee0b4a1a53c5d2538
Megan Nguyen <mdptnguyen@gmail.com>
2016-11-28 01:19:04 -0800

    calculates arrival time inbetween airports

--- CMPS112/asg4/functions.pl
+++ CMPS112/asg4/functions.pl
@@ -25,7 +25,7 @@ match_airport(From, To, Name1, Name2, LatD1, LatM1, LonD1, LonM1, LatD2, LatM2, 
         LonM2 = LonM2a.
 
 
-% converts degrees, min to radians
+% converts degrees, min to radians. 1 min = 0.0166667 deg, 1 deg = 0.0174533 rad
 convert_to_radians(LatD1, LatM1, LonD1, LonM1, LatD2, LatM2, LonD2, LonM2, Lat1, Lon1, Lat2, Lon2) :-
         Lat1 is (LatD1 + (LatM1 * 0.0166667)) * 0.0174533,
         Lon1 is (LonD1 + (LonM1 * 0.0166667)) * 0.0174533,
@@ -33,13 +33,28 @@ convert_to_radians(LatD1, LatM1, LonD1, LonM1, LatD2, LatM2, LonD2, LonM2, Lat1,
         Lon2 is (LonD2 + (LonM2 * 0.0166667)) * 0.0174533.
 
 
-% haversine function from functions.pl example
+% haversine function from functions.pl example, gets distance between airports in miles
 haversine_radians( Lat1, Lon1, Lat2, Lon2, Distance ) :-
         Dlon is Lon2 - Lon1,
         Dlat is Lat2 - Lat1,
         A is sin( Dlat / 2 ) ** 2 + cos( Lat1 ) * cos( Lat2 ) * sin( Dlon / 2 ) ** 2,
         Dist is 2 * atan2( sqrt( A ), sqrt( 1 - A )),
         Distance is Dist * 3961.
+
+
+% calculates the arrival time with planes flying 500 mi/hr.
+% calculates duration in minutes and then adds & divides accordingly for arrival time
+calculate_arrival_time(DepHour, DepMin, Distance, ArrHour, ArrMin) :-
+        % convert departure time into minutes
+        DecimalMin is DepMin / 60,
+        DepartMin is (DepHour + DecimalMin) * 60,
+        % find duration of flight in minutes
+        DurationMin is Distance / 500 * 60,
+        % add duration of flight to the departure time
+        ArrivalMin is DepartMin + DurationMin,
+        RoundedMin is truncate(ArrivalMin),
+        ArrHour is RoundedMin // 60,
+        ArrMin is mod(RoundedMin, 60).
 
 
 fly(From, To) :-
@@ -63,6 +78,7 @@ get_departure_time(X, Y, time(HourA, MinA)) :-
         match_airport(X, Y, Name1, Name2, LatD1, LatM1, LonD1, LonM1, LatD2, LatM2, LonD2, LonM2),
         convert_to_radians(LatD1, LatM1, LonD1, LonM1, LatD2, LatM2, LonD2, LonM2, Lat1, Lon1, Lat2, Lon2),
         haversine_radians(Lat1, Lon1, Lat2, Lon2, Distance),
+        calculate_arrival_time(HourB, MinB, Distance, ArrHour, ArrMin),
         print('depart '),
         print(X),
         print(' '),
@@ -76,6 +92,10 @@ get_departure_time(X, Y, time(HourA, MinA)) :-
         print(Y),
         print(' '),
         print(Name2),
+        print(' '),
+        print(ArrHour),
+        print(':'),
+        print(ArrMin),
         nl,
         print(Distance).
 
