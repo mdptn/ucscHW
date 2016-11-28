6cfebf5caebfcf11cd3674d79d035be355a40c61
Megan Nguyen <mdptnguyen@gmail.com>
2016-11-27 21:54:21 -0800

    finds the direct paths

--- CMPS112/asg4/functions.pl
+++ CMPS112/asg4/functions.pl
@@ -8,10 +8,30 @@
 
 :- initialization(main).
 
-hello :-
-        write( 'Hello, World!' ), 
-   nl.
+
+fly(From, To) :-
+        get_departure_time(From, To, time(0, 0)).
+
+
+
+% if the start destination is the same as the end, zero-fly.
+get_departure_time(X, X) :-
+        print('Error: Zero-fly query - You are already at your destination.'),
+        nl,
+        halt.
+
+
+
+% finds direct flights between locations
+get_departure_time(X, Y, time(HourA, MinA)) :-
+        flight(X, Y, time(HourB, MinB)),
+        A is HourA + MinA/60,
+        B is HourB + MinB/60,
+        A < B,
+        print(flight(X, Y, time(HourB, MinB))),
+        nl.
+
+
 
 main :-
-        hello,
-        halt.
+        [database].
