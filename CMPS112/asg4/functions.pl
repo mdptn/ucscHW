a8a07a6059b1ec5462e3c22f03cebeabb10a3a98
Megan Nguyen <mdptnguyen@gmail.com>
2016-11-27 23:52:22 -0800

    can calculate distance between airports

--- CMPS112/asg4/functions.pl
+++ CMPS112/asg4/functions.pl
@@ -9,17 +9,41 @@
 :- initialization(main).
 
 
+% matches the airport abbreviation with the airport information
+match_airport(From, To, Name1, Name2, LatD1, LatM1, LonD1, LonM1, LatD2, LatM2, LonD2, LonM2) :-
+        airport(From, Name1a, degmin(LatD1a, LatM1a), degmin(LonD1a, LonM1a)),
+        airport(To, Name2a, degmin(LatD2a, LatM2a), degmin(LonD2a, LonM2a)),
+        Name1 = Name1a,
+        Name2 = Name2a,
+        LatD1 = LatD1a,
+        LatM1 = LatM1a,
+        LonD1 = LonD1a,
+        LonM1 = LonM1a,
+        LatD2 = LatD2a,
+        LatM2 = LatM2a,
+        LonD2 = LonD2a,
+        LonM2 = LonM2a.
+
+
+% converts degrees, min to radians
+convert_to_radians(LatD1, LatM1, LonD1, LonM1, LatD2, LatM2, LonD2, LonM2, Lat1, Lon1, Lat2, Lon2) :-
+        Lat1 is (LatD1 + (LatM1 * 0.0166667)) * 0.0174533,
+        Lon1 is (LonD1 + (LonM1 * 0.0166667)) * 0.0174533,
+        Lat2 is (LatD2 + (LatM2 * 0.0166667)) * 0.0174533,
+        Lon2 is (LonD2 + (LonM2 * 0.0166667)) * 0.0174533.
+
+
+% haversine function from functions.pl example
+haversine_radians( Lat1, Lon1, Lat2, Lon2, Distance ) :-
+        Dlon is Lon2 - Lon1,
+        Dlat is Lat2 - Lat1,
+        A is sin( Dlat / 2 ) ** 2 + cos( Lat1 ) * cos( Lat2 ) * sin( Dlon / 2 ) ** 2,
+        Dist is 2 * atan2( sqrt( A ), sqrt( 1 - A )),
+        Distance is Dist * 3961.
+
+
 fly(From, To) :-
         get_departure_time(From, To, time(0, 0)).
-
-
-match_airport(From, To, Name1, Name2) :-
-        airport(From, Name1a, degmin(_, _), degmin(_, _)),
-        airport(To, Name2a, degmin(_, _), degmin(_, _)),
-        Name1 = Name1a,
-        Name2 = Name2a.
-
-
 
 
 % if the start destination is the same as the end, zero-fly.
@@ -36,7 +60,9 @@ get_departure_time(X, Y, time(HourA, MinA)) :-
         A is HourA + MinA/60,
         B is HourB + MinB/60,
         A < B,
-        match_airport(X, Y, Name1, Name2),
+        match_airport(X, Y, Name1, Name2, LatD1, LatM1, LonD1, LonM1, LatD2, LatM2, LonD2, LonM2),
+        convert_to_radians(LatD1, LatM1, LonD1, LonM1, LatD2, LatM2, LonD2, LonM2, Lat1, Lon1, Lat2, Lon2),
+        haversine_radians(Lat1, Lon1, Lat2, Lon2, Distance),
         print('depart '),
         print(X),
         print(' '),
@@ -50,7 +76,8 @@ get_departure_time(X, Y, time(HourA, MinA)) :-
         print(Y),
         print(' '),
         print(Name2),
-        nl.
+        nl,
+        print(Distance).
 
 
 
